39b98c54a8bc394afeea4df60cfe5e616718ad27
abduholiqovmuhammadzohir <135995518+abduholiqovmuhammadzohir@users.noreply.github.com>
2024-05-30 09:39:02 +0500

    start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button className="btn btn-warning">Warning</button>
+    </div>
+  )
+}
+
+export default App
